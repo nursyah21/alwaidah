b02ce6c15b258be0d503efc568fe8ae8506d740c
nursyahjaya <nursyahjaya@gmail.com>
2023-02-06 05:12:59 +0700

    init

---
+++ as.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <img src="/profie.jpeg" />
+      Al waidah
+    </> 
+  )
+}
+
+export default App
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import profile from './assets/profile.jpeg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img src={profile} className="logo" /><br/>
+      Al Waidah
+    </>
+  )
+}
+
+export default App
