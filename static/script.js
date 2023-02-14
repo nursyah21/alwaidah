7b2d3748973fcefc5e7ad1a0b22f070e5b7867d0
nursyahjaya <nursyahjaya@gmail.com>
2023-02-14 16:24:29 +0700

    update

---
+++ static/script.js
@@ -0,0 +1,8 @@
+function navFunc(){
+  document.getElementsByClassName('navbar-burger')[0].classList.toggle('is-active')
+  document.getElementsByClassName('navbar-menu')[0].classList.toggle('is-active')
+}
+
+const listNav = [
+  "Pelatih", "Recording", "Berita", "Gambus Modern", "Gambus Tradisional"
+]
